c9f4ddd485c1c13930de72d3b641cdd30f474c93
Samuel <samuel.kamawira@student.moringaschool.com>
2024-08-17 16:26:07 +0300

    first

--- App/src/components/Header.jsx
+++ App/src/components/Header.jsx
@@ -16,12 +16,12 @@ const Header = () => {
       <nav className="container mx-auto flex justify-between items-center py-4 px-4 md:px-0 text-blue-900">
         <h1 className="text-2xl font-bold">G.A.S <span className='text-red-700'>0.D</span></h1>
         <ul className="flex space-x-2">
-          <li><Link to="/" className="flex items-center hover:text-gray-300 transition-colors duration-200"><HomeIcon className="w-5 h-5 mr-1"/>Home</Link></li>
+          <li><Link to="/" className="flex items-center hover:text-gray-300 transition-colors duration-200"><HomeIcon className="w-5 h-5 mr-1"/></Link></li>
           
           {/* Dropdown Menu for Catalog */}
           <li className="relative group">
             <Link to="/catalog" className="flex items-center hover:text-gray-300 transition-colors duration-200">
-              <ShoppingBagIcon className="w-5 h-5 mr-1"/>Catalog
+              <ShoppingBagIcon className="w-5 h-5 mr-1"/>
             </Link>
             <ul className="absolute hidden group-hover:block bg-white text-black p-3 rounded-lg shadow-lg mt-1 transition-all duration-300">
               <li><Link to="/catalog/brakes" className="block px-4 py-2 hover:bg-gray-200 rounded">Brakes</Link></li>
@@ -33,7 +33,7 @@ const Header = () => {
           {/* Dropdown Menu for Feedback */}
           <li className="relative group">
             <Link to="/feedback" className="flex items-center hover:text-gray-300 transition-colors duration-200">
-              <ChatBubbleLeftEllipsisIcon className="w-5 h-5 mr-1"/>Feedback
+              <ChatBubbleLeftEllipsisIcon className="w-5 h-5 mr-1"/>
             </Link>
             <ul className="absolute hidden group-hover:block bg-white text-black p-3 rounded-lg shadow-lg mt-1 transition-all duration-300">
               <li><Link to="/feedback/submit" className="block px-4 py-2 hover:bg-gray-200 rounded">Submit Feedback</Link></li>
@@ -44,7 +44,7 @@ const Header = () => {
           {/* Dropdown Menu for Profile */}
           <li className="relative group">
             <Link to="/profile" className="flex items-center hover:text-gray-300 transition-colors duration-200">
-              <UserCircleIcon className="w-5 h-5 mr-1"/>Profile
+              <UserCircleIcon className="w-5 h-5 mr-1"/>
             </Link>
             <ul className="absolute hidden group-hover:block bg-white text-black p-3 rounded-lg shadow-lg mt-1 transition-all duration-300">
               <li><Link to="/profile" className="block px-4 py-2 hover:bg-gray-200 rounded">View Profile</Link></li>
